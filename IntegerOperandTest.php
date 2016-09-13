214ece78e3b301568454a046f3cee343cc0b8665
rnd-pai <ksorn@kku.ac.th>
2016-09-13 16:14:12 +0700

    Added Integer operand testing.

---
+++ IntegerOperandTest.php
@@ -0,0 +1,22 @@
+<?php
+class IntegerOperand{
+
+	function __construct($value)
+	{
+		$this->value = $value;
+	}
+
+	function toString(){
+		return $this->value;
+	}
+}
+
+
+class IntegerOperandTest extends PHPUnit_Framework_TestCase 
+{
+	function testShouldBeOne(){
+		$operand = new IntegerOperand(1);
+		$this->assertEquals("1",$operand->toString()); 
+	}
+}
+
