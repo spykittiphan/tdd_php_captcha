07d881341db05b8959b5b08a11f811f88cad2271
rnd-pai <ksorn@kku.ac.th>
2016-09-13 16:00:24 +0700

    String operand TWO

--- StringOperandTest.php
+++ StringOperandTest.php
@@ -1,14 +1,29 @@
 <?php
-class Operand {
+
+class Operand { 
+	function __construct($number) 
+	{
+		$this->number = $number;
+	} 
 	function toString(){
+		if($this->number==2){
+			return "TWO";
+		}
 		return "ONE";
 	}
+
 }
 class StringOperandTest extends PHPUnit_Framework_TestCase
 {
-	function testStringOperand(){
+	function testStringOperandONE(){
 		$stringOperand = new Operand(1);
 		$this->assertEquals('ONE',$stringOperand->toString());
 	}
+
+	function testStringOperandTWO(){
+		$stringOperand = new Operand(2);
+		$this->assertEquals('TWO',$stringOperand->toString());
+	}
+
 }
 ?>
