f57bc18c93fa19c2d890f541b88868ec9ba901ff
rnd-pai <ksorn@kku.ac.th>
2016-09-13 11:22:34 +0700

    Finish Left

---
+++ CaptchaTest.php
@@ -0,0 +1,45 @@
+<?php
+class Captcha
+{
+	function __construct($pattern, $left, $operator, $right)
+	{
+		$this->left = $left;
+	}
+
+	function left()
+	{
+		$_left =[1=>"ONE",2=>"TWO",9=>"NINE"];
+		
+		return $_left[$this->left];
+	}
+}
+
+class CaptchaTest extends PHPUnit_Framework_TestCase 
+{
+	private $DUMMY_PATTERN = 1;
+	private $DUMMY_OPERATOR = 1;
+	private $DUMMY_RIGHT = 1;
+
+	function testLeftShouldBeNINE()
+	{
+		$captcha = new Captcha($this->DUMMY_PATTERN,9,$this->DUMMY_OPERATOR,$this->DUMMY_RIGHT);
+		$this->assertEquals("NINE",$captcha->left());
+
+	}
+
+	function testLeftShouldBeONE() 
+	{
+
+		$captcha = new Captcha($this->DUMMY_PATTERN, 1, $this->DUMMY_OPERATOR, 
+			$this->DUMMY_RIGHT);
+		$this->assertEquals("ONE", $captcha->left());
+	}
+
+	function testLeftShouldBeTWO() 
+	{
+		$captcha = new Captcha($this->DUMMY_PATTERN, 2, $this->DUMMY_OPERATOR, $this->DUMMY_RIGHT);
+		$this->assertEquals("TWO", $captcha->left());
+	}
+
+
+}
