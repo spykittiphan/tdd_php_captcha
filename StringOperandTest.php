92e3443a755f39e3c597bd330329761be80f13bb
rnd-pai <ksorn@kku.ac.th>
2016-09-13 16:05:10 +0700

    Refactor

--- StringOperandTest.php
+++ StringOperandTest.php
@@ -1,17 +1,15 @@
 <?php
 
 class StringOperand { 
+	private $NUMBER_TO_TEXT = [1=>"ONE",2=>"TWO",3=>"THREE",4=>"FOUR",5=>"FIVE",6=>"SIX",7=>"SEVEN",8=>"EIGHT",9=>"NINE"];
+
 	function __construct($number) 
 	{
 		$this->number = $number;
 	} 
 	function toString(){
-		if($this->number==2){
-			return "TWO";
-		}else if($this->number==9){
-			return "NINE";
-		}
-		return "ONE";
+
+		return $this->NUMBER_TO_TEXT[$this->number];
 	}
 
 }
